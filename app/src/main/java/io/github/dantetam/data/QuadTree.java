6f6b6c341c65ce4b0a925ddee09a47ca072c0610
dantetam <dante.tam1@gmail.com>
2016-06-16 10:22:07 -0700

    World tree data structure

--- app/src/main/java/io/github/dantetam/data/QuadTree.java
+++ app/src/main/java/io/github/dantetam/data/QuadTree.java
@@ -13,52 +13,19 @@ public abstract class QuadTree<K, V> {
 
     public Node<K, V> root;
 
-    public Node group(K[][] keys, Node[][] data) {
-        while (true) {
-            K[][] newKeys = (K[][]) new Object[len(keys.length, 2)][len(keys[0].length, 2)];
-            Node[][] newData = (Node[][]) new Object[len(data.length, 2)][len(data[0].length, 2)];
-            for (int i = 0; i < newKeys.length; i++) {
-                for (int j = 0; j < newKeys[0].length/2; j++) {
-                    ArrayList<K> neighbors = new ArrayList<K>();
-                    int[][] localKeys = new int[][]{
-                            new int[]{i * 2, j * 2},
-                            new int[]{i * 2, j * 2 + 1},
-                            new int[]{i * 2 + 1, j * 2},
-                            new int[]{i * 2 + 1, j * 2 + 1}
-                    };
-                    //keys[i*2][j*2], keys[i*2][j*2 + 1], keys[i*2 + 1][j*2], keys[i*2 + 1][j*2 + 1]
-                    for (int k = 0; k < localKeys.length; k++) {
-                        if (inBounds(keys, localKeys[i][0], localKeys[i][1])) {
-                            neighbors.add(keys[localKeys[i][0]][localKeys[i][1]]);
-                        }
-                    }
-                    if (inBounds(keys, i*2, j*2)) neighbors.add(keys[i*2][j*2]);
-                    if (inBounds(keys, i*2, j*2 + 1)) neighbors.add(keys[i*2][j*2 + 1]);
-                    if (inBounds(keys, i*2 + 1, j*2)) neighbors.add(keys[i*2 + 1][j*2]);
-                    if (inBounds(keys, i*2 + 1, j*2 + 1)) neighbors.add(keys[i*2 + 1][j*2 + 1]);
-                    newKeys[i][j] = combine(localKeys);
-                    newData[i][j] = new Node(neighbors);
-                }
-            }
-            keys = newKeys;
-            data = newData;
-            if (keys.length == 1 && keys[0].length == 1) {
-                return data[0][0];
-            }
-        }
-    }
-    private int len(int n, int mod) {
+    public abstract Node group(K[][] keys, Node[][] data);
+    protected int len(int n, int mod) {
         int extra = n%mod > 0 ? 1 : 0;
         return n/mod + extra;
     }
-    private boolean inBounds(Object[][] table, int r, int c) {
+    protected boolean inBounds(Object[][] table, double r, double c) {
         if (table.length == 0) {
             throw new IllegalArgumentException("Degenerate table");
         }
         return r >= 0 && c >= 0 && r < table.length && c < table[0].length;
     }
 
-    public abstract K combine(List<K> data);
+    public abstract K combine(K[] data);
 
     /*public int[] combine(int[][] data) {
         double[] temp = {0,0}
---
+++ app/src/main/java/io/github/dantetam/data/WorldTree.java
@@ -0,0 +1,58 @@
+package io.github.dantetam.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.dantetam.world.Tile;
+
+/**
+ * Created by Dante on 6/16/2016.
+ */
+public class WorldTree extends QuadTree<double[], Tile>{
+
+    @Override
+    public Node group(double[][][] keys, Node[][] data) {
+        while (true) {
+            double[][][] newKeys = new double[len(keys.length, 2)][len(keys[0].length, 2)][];
+            Node[][] newData = (Node[][]) new Object[len(data.length, 2)][len(data[0].length, 2)];
+            for (int i = 0; i < newKeys.length; i++) {
+                for (int j = 0; j < newKeys[0].length/2; j++) {
+                    ArrayList<double[]> neighbors = new ArrayList<double[]>();
+                    double[][] localKeys = new double[][]{
+                            new double[]{i * 2, j * 2},
+                            new double[]{i * 2, j * 2 + 1},
+                            new double[]{i * 2 + 1, j * 2},
+                            new double[]{i * 2 + 1, j * 2 + 1}
+                    };
+                    //keys[i*2][j*2], keys[i*2][j*2 + 1], keys[i*2 + 1][j*2], keys[i*2 + 1][j*2 + 1]
+                    for (int k = 0; k < localKeys.length; k++) {
+                        if (inBounds(keys, localKeys[i][0], localKeys[i][1])) {
+                            neighbors.add(keys[(int)localKeys[i][0]][(int)localKeys[i][1]]);
+                        }
+                    }
+                    /*if (inBounds(keys, i*2, j*2)) neighbors.add(keys[i*2][j*2]);
+                    if (inBounds(keys, i*2, j*2 + 1)) neighbors.add(keys[i*2][j*2 + 1]);
+                    if (inBounds(keys, i*2 + 1, j*2)) neighbors.add(keys[i*2 + 1][j*2]);
+                    if (inBounds(keys, i*2 + 1, j*2 + 1)) neighbors.add(keys[i*2 + 1][j*2 + 1]);*/
+                    newKeys[i][j] = combine(localKeys);
+                    newData[i][j] = new Node(neighbors);
+                }
+            }
+            keys = newKeys;
+            data = newData;
+            if (keys.length == 1 && keys[0].length == 1) {
+                return data[0][0];
+            }
+        }
+    }
+
+    @Override
+    public double[] combine(double[][] data) {
+        double[] temp = {0,0};
+        for (int i = 0; i < data.length; i++) {
+            temp[0] += data[i][0];
+            temp[1] += data[i][1];
+        }
+        return new double[]{temp[0]/data.length, temp[1]/data.length};
+    }
+}
